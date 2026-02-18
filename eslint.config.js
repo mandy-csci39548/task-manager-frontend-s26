204dfef072ce022cc2eca045dedb4ef983c600f6
Mandy <mandy39548@gmail.com>
2026-02-18 18:53:04 -0500

    init task manager

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import { defineConfig, globalIgnores } from "eslint/config";
+import react from "eslint-plugin-react";
+
+export default defineConfig([
+  globalIgnores(["dist"]),
+  {
+    files: ["**/*.{js,jsx}"],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+      react.configs.flat.recommended,
+      react.configs.flat["jsx-runtime"],
+    ],
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        ecmaFeatures: { jsx: true },
+        sourceType: "module",
+      },
+    },
+    rules: {
+      "no-unused-vars": ["warn", { varsIgnorePattern: "^[A-Z_]" }],
+      "no-console": "warn",
+    },
+  },
+]);
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+const App = () => {
+  const name = 'Mandy';
+
+  return <div>Hello</div>;
+};
+
+export default App;
